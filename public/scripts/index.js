78d952dca582b79e27effc3c43a9e7410f2590bf
hkdnet <hkdnet@users.noreply.github.com>
2016-01-23 21:17:53 +0900

    chart部分追加

---
+++ public/scripts/index.js
@@ -0,0 +1,42 @@
+'use strict';
+var dataset = [
+  { name: '三浦あずさ', age: 21, height: 168, bust: 91, waist: 59, hip: 86 },
+  { name: '四条貴音', age: 18, height: 169, bust: 90, waist: 62, hip: 92 },
+  { name: '星井美希', age: 15, height: 161, bust: 86, waist: 55, hip: 83 },
+  { name: '秋月律子', age: 19, height: 156, bust: 85, waist: 57, hip: 85 },
+  { name: '天海春香', age: 17, height: 158, bust: 83, waist: 56, hip: 82 },
+  { name: '我那覇響', age: 16, height: 152, bust: 83, waist: 56, hip: 80 },
+  { name: '萩原雪歩', age: 17, height: 155, bust: 81, waist: 56, hip: 81 },
+  { name: '双海亜美', age: 13, height: 158, bust: 78, waist: 55, hip: 77 },
+  { name: '双海真美', age: 13, height: 158, bust: 78, waist: 55, hip: 77 },
+  { name: '水瀬伊織', age: 15, height: 153, bust: 77, waist: 54, hip: 79 },
+  { name: '菊地真', age: 17, height: 159, bust: 75, waist: 57, hip: 78 },
+  { name: '高槻やよい', age: 14, height: 145, bust: 74, waist: 54, hip: 78 },
+  { name: '如月千早', age: 16, height: 162, bust: 72, waist: 55, hip: 78 }
+];
+window.addEventListener('DOMContentLoaded', function() {
+(function($, $$, undefined) {
+  var chartSize = {
+    width: 300,
+    height: 300
+  };
+  var minHeight = _.minBy(dataset, function(d) { return d.height; }).height;
+  var maxHeight = _.maxBy(dataset, function(d) { return d.height; }).height;
+  var heightScale = function(height) {
+    var minmax = maxHeight - minHeight;
+    return (height - minHeight) * (chartSize.width - 20) / (minmax) + 10;
+  };
+  var minBust = _.minBy(dataset, function(d) { return d.bust; }).bust;
+  var maxBust = _.maxBy(dataset, function(d) { return d.bust; }).bust;
+  var bustScale = function(bust) {
+    var minmax = maxBust - minBust;
+    return (bust - minBust) * (chartSize.height - 20) / minmax + 10;
+  };
+  var svg = d3.select('#chart').append('svg').attr(chartSize);
+  svg.selectAll('circle').data(dataset).enter().append('circle').attr({
+    cx: function(d) { return heightScale(d.height); },
+    cy: function(d) { return bustScale(d.bust); },
+    r: 5
+  });
+})(document.querySelector.bind(document), document.querySelectorAll.bind(document));
+});
